a7c5bdd865933ca155981501cc40ea42d5a6e3cc
Daniel Diaz Medrano <danieldiaz129@gmail.com>
2018-02-14 18:32:32 -0800

    Created the first steps for the search bar to work.

--- js/main.js
+++ js/main.js
@@ -1,6 +1,17 @@
 $(document).ready(() => {
     $('#searchForm').on('submit', (e) => {
-        console.log($('#searchText').val());
+        let searchText = $('#searchText').val();
+        getShows(searchText);
         e.preventDefault();
     });
 });
+
+function getShows(searchText) {
+    axios.get('https://api.themoviedb.org/3/search/tv?api_key=fb6a1d3f38c3d97f67df6d141f936f29&language=en-US&query=' + searchText + '&page=1')
+        .then((response) => {
+            console.log(response);
+        })
+        .catch((err) => {
+            console.log(err);
+        })
+}
