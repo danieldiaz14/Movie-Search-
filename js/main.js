98a860a18b143bd93fa1a68420821d98ad2cd022
Daniel Diaz Medrano <danieldiaz129@gmail.com>
2018-02-14 22:25:44 -0800

    Everything works

--- js/main.js
+++ js/main.js
@@ -1,4 +1,5 @@
 $(document).ready(() => {
+    getPopular();
     $('#searchForm').on('submit', (e) => {
         let searchText = $('#searchText').val();
         getShows(searchText);
@@ -6,20 +7,43 @@ $(document).ready(() => {
     });
 });
 
+function getPopular() {
+    axios.get('https://api.themoviedb.org/3/discover/tv?api_key=fb6a1d3f38c3d97f67df6d141f936f29&language=en-US&sort_by=popularity.desc&page=1&timezone=America%2FNew_York&include_null_first_air_dates=false')
+        .then((response) => {
+            let shows = response.data.results;
+            console.log(shows);
+            let output = '';
+            for(let i = 0; i < shows.length; i++) {
+                output += `
+                    <div class="col-md-3">
+                        <div class="well text-center">
+                            <img src="${"https://image.tmdb.org/t/p/w500" + shows[i].poster_path}">
+                            <h5>${shows[i].name}</h5>
+                            <a onclick="showSelected('${shows[i].id}')" class="btn btn-primary" href="#">Show Details</a>
+                        </div>
+                    </div>
+                `;
+            }
+            $('#shows').html(output);
+        })
+        .catch((err) => {
+            console.log(err);
+        });
+}
+
 function getShows(searchText) {
     axios.get('https://api.themoviedb.org/3/search/tv?api_key=fb6a1d3f38c3d97f67df6d141f936f29&language=en-US&query=' + searchText + '&page=1')
         .then((response) => {
             //console.log(response);
-            let shows = response.data;
-            console.log(shows.results[5]);
+            let shows = response.data.results;
             let output = '';
-            for(let i = 0; i < shows.results.length; i++) {
+            for(let i = 0; i < shows.length; i++) {
                 output += `
                     <div class="col-md-3">
                         <div class="well text-center">
-                            <img src="${"https://image.tmdb.org/t/p/w500" + shows.results[i].poster_path}">
-                            <h5>${shows.results[i].name}</h5>
-                            <a onclick="showSelected('${shows.results[i].id}')" class="btn btn-primary" href="#">Show Details</a>
+                            <img src="${"https://image.tmdb.org/t/p/w500" + shows[i].poster_path}">
+                            <h5>${shows[i].name}</h5>
+                            <a onclick="showSelected('${shows[i].id}')" class="btn btn-primary" href="#">Show Details</a>
                         </div>
                     </div>
                 `;
@@ -37,13 +61,21 @@ function showSelected(id) {
     return false;
 }
 
+function check(input) {
+    if (input.length <= 0) {
+        return 'No idea';
+    }
+    return input[0].name;
+}
+
 function getShow() {
     let showId = sessionStorage.getItem('showId');
     axios.get('https://api.themoviedb.org/3/tv/' + showId + '?api_key=fb6a1d3f38c3d97f67df6d141f936f29&language=en-US')
         .then((response) => {
             console.log(response);
             let show = response.data;
-
+            let genre = check(show.genres);
+            let createdby = check(show.created_by);
             let output =`
             <div class="row">
             <div class="col-md-4">
@@ -52,10 +84,10 @@ function getShow() {
             <div class="col-md-8">
                 <h2>${show.name}</h2>
                 <ul class="list-group">
-                <li class="list-group-item"><strong>Genre:</strong> ${show.genres[0].name}</li>
+                <li class="list-group-item"><strong>Genre:</strong> ${genre}</li>
                 <li class="list-group-item"><strong>Released:</strong> ${show.first_air_date}</li>
                 <li class="list-group-item"><strong>Rated:</strong> ${show.vote_average}</li>
-                <li class="list-group-item"><strong>Director:</strong> ${show.created_by[0].name}</li>
+                <li class="list-group-item"><strong>Director:</strong> ${createdby}</li>
                 <li class="list-group-item"><strong>Seasons:</strong> ${show.seasons.length}</li>
                 <li class="list-group-item"><strong>Last Air Date:</strong> ${show.last_air_date}</li>
                 </ul>
