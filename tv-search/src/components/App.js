3a8fbf6addd3fe5ce85ded93116776710c0ced40
Daniel Medrano <daniel.medrano@ASLEEPCOUCHSKI.workdayinternal.com>
2018-12-07 13:48:13 -0800

    Api search requests working

--- tv-search/src/components/App.js
+++ tv-search/src/components/App.js
@@ -2,14 +2,23 @@ import './main.css';
 import React from 'react';
 import Home from './Home';
 import SearchBar from './SearchBar';
-
+import MovieDB from '../api/MovieDB';
 class App extends React.Component {
-
+    state = {
+        movies: []
+    };
+    onSubmit = async (term) => {
+        const response = await MovieDB.get('/search/tv',{
+            params: {api_key:'b8ef17bd40019759c9b93654e682a1a0', language:'en-US', query: term, page:1}
+        });
+        console.log(response.data.results);
+        this.setState({movies: response.data.results});
+    };
     render() {
         return (
             <div>
                 <Home/>
-                <SearchBar/>
+                <SearchBar onSubmit={this.onSubmit}/>
             </div>
         )
     }
--- tv-search/src/components/SearchBar.js
+++ tv-search/src/components/SearchBar.js
@@ -2,13 +2,21 @@ import React from 'react';
 
 
 class SearchBar extends React.Component {
+    state = {
+        term: ''
+    };
+
+    updateSearch = (e) => {
+        e.preventDefault();
+        this.props.onSubmit(this.state.term);
+    }
     render() {
         return (
             <div className="container">
                 <div className="jumbotron">
                     <h3 className="text-center">Search for any TV show!</h3>
-                    <form className="searchForm">
-                        <input type="text" className="form-control" placeholder="Search any tv show"></input>
+                    <form className="searchForm" onSubmit={this.updateSearch}>
+                        <input type="text" value={this.state.term} onChange={e => this.setState({term: e.target.value})} className="form-control" placeholder="Search any tv show"></input>
                     </form>
                 </div>
             </div>
