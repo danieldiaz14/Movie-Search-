fe1bebf58c9d0bde353eb82579b87527177b204c
Daniel Medrano <daniel.medrano@ASLEEPCOUCHSKI.workdayinternal.com>
2018-12-07 16:09:15 -0800

    Better default image

--- tv-search/src/components/ShowList.js
+++ tv-search/src/components/ShowList.js
@@ -1,12 +1,13 @@
+import Img from './placeholder1.jpg';
 import React from 'react';
-
 const ShowList = (props) => {
     const baseImg = 'https://image.tmdb.org/t/p/w500'
     const shows = props.shows.map((show) => {
+        const img = show.poster_path != null ? `${baseImg}${show.poster_path}` : Img;
         return (
             <div key={show.id} className="col-md-3">
                 <div className="well text-center">
-                <img alt={show.overview} src={`${baseImg}${show.poster_path}`}/>
+                <img alt={show.overview} src={img}/>
                 <h5>{show.name}</h5>
                 </div>
             </div>
