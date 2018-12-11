ddc110a47e78eff98d18e42b375d7fc71ddd20f7
Daniel Diaz Medrano <danieldiaz129@gmail.com>
2018-12-10 20:29:27 -0800

    Details about selected showing

--- tv-search/src/components/ShowDetail.js
+++ tv-search/src/components/ShowDetail.js
@@ -4,6 +4,9 @@ import React from 'react';
 const ShowDetail = ({info}) => {
     const baseImg = 'https://image.tmdb.org/t/p/w500';
     const img = info.poster_path != null ? `${baseImg}${info.poster_path}` : Img;
+    const backup = 'information not provided';
+    const genre = info.genres.length > 0 ? info.genres[0].name : backup;
+    const director = info.created_by.length > 0 ? info.created_by[0].name : backup;
     return (
         <div>
             <div className="row">
@@ -13,7 +16,12 @@ const ShowDetail = ({info}) => {
                 <div className="col-md-8">
                 <h2>{info.name}</h2>
                 <ul className="list-group">
-
+                <li className="list-group-item"><strong>Genre:</strong> {genre}</li>
+                <li className="list-group-item"><strong>Released:</strong> {info.first_air_date || backup}</li>
+                <li className="list-group-item"><strong>Rated:</strong> {info.vote_average || backup}</li>
+                <li className="list-group-item"><strong>Director:</strong> {director}</li>
+                <li className="list-group-item"><strong>Seasons:</strong> {info.seasons.length || backup}</li>
+                <li className="list-group-item"><strong>Last Air Date:</strong> {info.last_air_date || backup}</li>
                 </ul>
             </div>
             </div>
